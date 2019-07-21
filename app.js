166a25a90777b82cda3e0c8c9e74b32a83c1cb2d
Jennifer Perez <jenniferperez@pursuit.org>
2019-07-21 12:43:22 -0400

    added packages

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const admin = require('./firebase');
+
+
+//define routes
+const clothesRouter = require('./routes/clothes');
+
+
+
+// -------- MIDDLEWARE
+app.use(cors());
+// parse application/x-www-form-urlencoded
+//cors calls backend from frontend or else it will block u
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+//routes
+app.use('/user',userRoutes)
+
+
+
+app.use((err, req, res, next) => {
+  res.status(400).json({error: err.toString()});
+});
+
+
+// -------- ROUTES
+app.get('/', (req, res) => {
+  res.json('Hello world');
+})
+
+app.post('/', clothesRouter)
+
+
+
+
+module.exports={app}
