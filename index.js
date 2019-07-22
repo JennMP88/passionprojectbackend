38081c063f80f4a2d34176dda287c2af5dea7f04
Jennifer Perez <jenniferperez@pursuit.org>
2019-07-22 18:17:25 -0400

    made changes to seed file

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+//define routes
 const express = require('express');
 const app = express();
 const port = 8080;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const admin = require('./firebase');
-
-
-//define routes
+const admin = require('./firebase');
 const commentsRouter = require('./routes /comments');
-
 
 
 // -------- MIDDLEWARE
@@ -18,6 +15,22 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+
+const checkFirebaseToken = (req, res, next) => {
+  const { token } = req.body;
+
+  admin.auth().verifyIdToken(token)
+    .then(function(decodedToken) {
+      var uid = decodedToken.uid;
+      // decodedToken=decodedToken.uid;
+      next();
+    }).catch(function(error) {
+      // Handle error
+      res.json('ERROR!!!!')
+    });
+}
+
 
 
 // routes
@@ -35,6 +48,19 @@ app.get('/comment', (req, res) => {
   res.json('Hello world');
 })
 
+// //protected: uses firebase middleware
+// app.post('/unprotected', (req, res) => {
+//   const { email, id } = req.body;
+//   res.json(`Welcome! You are user ${email} with ${id}`);
+// })
+
+// app.post('/protected', checkFirebaseToken, 
+// //,bouncer2,
+// (req, res) => {
+//   res.json(`This is some sensitive data`);
+// })
+
+//-----------------------
 app.use('/comment', commentsRouter); 
 
 app.listen(port, ()=>{
@@ -42,3 +68,78 @@ app.listen(port, ()=>{
 } )
 
 module.exports={app}
+
+
+
+//-------------------------------------------------------------THERE
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const admin = require('./firebase');
+
+// // -------- MIDDLEWARE
+// app.use(cors());
+// // parse application/x-www-form-urlencoded
+// //cors calls backend from frontend or else it will block u
+// app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/json
+// app.use(bodyParser.json())
+
+// const checkFirebaseToken = (req, res, next) => {
+//   const { token } = req.body;
+
+//   admin.auth().verifyIdToken(token)
+//     .then(function(decodedToken) {
+//       var uid = decodedToken.uid;
+//       // decodedToken=decodedToken.uid;
+//       next();
+//     }).catch(function(error) {
+//       // Handle error
+//       res.json('ERROR!!!!')
+//     });
+// }
+
+
+
+// //if you want to delete a user 
+// //delete/user/jen you can also use the function below and do a check
+
+// //--------------------------------------------------
+
+// //how do i check if this is the user who should be performing a particular action?
+// // decodedToken=decodedToken.uid;
+// //get the users uid
+// // const uidInPostgress='';
+// // const uidindecodedtken=req.decodedToken.uid;            //-------- look above 
+// // if (uidInPostgress===uidindecodedtken){
+// //   next();
+// //  } else{
+// //     res.json({error:'you are not the valid user'})
+// //   }
+
+
+
+
+// //const bouncer2=()={}
+// // -------- ROUTES
+// app.get('/', (req, res) => {
+//   res.json('Hello world');
+// })
+
+// //protected: uses firebase middleware
+// app.post('/unprotected', (req, res) => {
+//   const { email, id } = req.body;
+//   res.json(`Welcome! You are user ${email} with ${id}`);
+// })
+
+// app.post('/protected', checkFirebaseToken, 
+// //,bouncer2,
+// (req, res) => {
+//   res.json(`This is some sensitive data`);
+// })
+
+// app.listen(port, () => {
+//   console.log('Server listening on port: '+port);
+// })
--- seed.sql
+++ seed.sql
@@ -14,12 +14,22 @@ CREATE TABLE comments (
     user_id INT REFERENCES users(id)
 );
 
+CREATE TABLE stories (
+  id SERIAL PRIMARY KEY,
+  stories_displayed VARCHAR NOT NULL,
+  user_id INT REFERENCES users(id) 
+);
+
 -- INSERT USERS
 INSERT INTO users (username) VALUES ('Jen');
 INSERT INTO users (username) VALUES ('Luke');
-INSERT INTO users (username) VALUES ('Marcos');
+INSERT INTO users (username) VALUES ('Marco');
 
+--INSERT STORY 
+  INSERT INTO stories (stories_displayed, user_id) VALUES ('i applied to Google, filled out a coding challenge and cursed a lady out on my way to my interview, next thing i knew, the same lady i cursed out was the person who interviewed me. we all know how that ended',1);
+  INSERT INTO stories (stories_displayed, user_id) VALUES ('just got rejected by Google',1);
+  INSERT INTO stories (stories_displayed, user_id) VALUES ('Left a job on hold while i negotiated a salary and next thing i knew they hired some one else',2);
 --INSER COMMENT
 INSERT INTO comments (comment_body, user_id) VALUES ('do not worry be happy',1);
 INSERT INTO comments (comment_body, user_id) VALUES ('every little thing is gonna be alright',1);
-INSERT INTO comments (comment_body, user_id) VALUES ('are you qouting bob marley?',2);
+INSERT INTO comments (comment_body, user_id) VALUES ('are you quoting bob marley?',2);
